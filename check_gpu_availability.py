2beb1f98c93132f179fa22078c141cb9378429ab
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-07-22 08:52:24 +0900

    自動整形でエラーになっていた部分の修正

--- check_gpu_availability.py
+++ check_gpu_availability.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         print(f"{count} GPUs detected.")
 
         device = torch.cuda.current_device()
-        print(f"Default GPU: {device}, name: {
-              torch.cuda.get_device_name(device)}")
+        print(f"Default GPU: {device}, "
+              + f"name: {torch.cuda.get_device_name(device)}")
 
     else:
         print("No GPUs detected.")
