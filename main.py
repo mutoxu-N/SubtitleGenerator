8ac3a06c23f0125e26e0bc73acbfa66de2b5051e
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-07-22 09:57:26 +0900

    LARGE_V3で実行 途中経過の実行時間を表示 ファイルに一括で書き込む

--- main.py
+++ main.py
@@ -4,23 +4,37 @@
 # Check CUDA version: https://pytorch.org/get-started/locally/
 
 import os
+import time
 from faster_whisper import WhisperModel
 from model import ModelSize
 
-MODEL = ModelSize.BASE
+MODEL = ModelSize.LARGE_V3
 INPUT_FILE = "sample.mp4"
 
-#  Initializing *.dll, but found *.dll already initialized. が出たら必要な環境変数
+#  Initializing *.dll, but found *.dll already initialized. が出ても処理が中断しないようにする環境変数
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+# UserWarning: `huggingface_hub` cache-system uses symlinks by default to efficiently... を隠すための環境変数
+os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
 
+start_time = time.perf_counter()
 model = WhisperModel(MODEL.value, device="cuda", compute_type="float16")
 # model = WhisperModel(MODEL.value, device="cuda", compute_type="int8_float16")
 
 segments, _ = model.transcribe(INPUT_FILE, beam_size=5)
+print("Whisper is Done!")
+end_time = time.perf_counter()
+elapsed = end_time - start_time
+elapsed_h = int(elapsed // 3600)
+elapsed_m = int(elapsed % 3600 // 60)
+elapsed_s = int(elapsed % 60)
+elapsed_ms = int(elapsed * 1000) % 1000
+print(" -> Elapsed time: "
+      + f"{elapsed_h:02d}:{elapsed_m:02d}:{elapsed_s:02d}.{elapsed_ms:03d}\n")
 
 # save to file
 text = ""
 with open("result.srt", "w") as f:
+    write = ""
     for i, segment in enumerate(segments):
         start_h = int(segment.start) // 3600
         start_m = int(segment.start) % 3600 // 60
@@ -32,17 +46,26 @@ with open("result.srt", "w") as f:
         end_s = int(segment.end) % 60
         end_ms = int(segment.end * 1000) % 1000
 
-        f.write(f"{i}\n")
-        f.write("%2d:%02d:%02d,%03d --> %2d:%02d:%02d,%03d\n" %
-                (start_h, start_m, start_s, start_ms, end_h, end_m, end_s, end_ms))
-        f.write(f"{segment.text}\n")
-        f.write("\n")
+        write += f"{i}\n"
+        write += f"{start_h:02d}:{start_m:02d}:{start_s:02d},{start_ms:03d} --> "
+        write += f"{end_h:02d}:{end_m:02d}:{end_s:02d},{end_ms:03d}\n"
+        write += f"{segment.text}\n"
+        write += "\n"
         text += segment.text
+    f.write(write)
 
 with open("result.out", "w") as f:
-    f.write(text + "\n\n")
-
+    write = text + "\n\n"
     for segment in segments:
-        f.write(f"[{segment.start}s -> {segment.end}s] {segment.text}\n")
-print(text)
-print("Done!")
+        write += f"[{segment.start}s -> {segment.end}s] {segment.text}\n"
+    f.write(write)
+
+print("All is Done!")
+end_time = time.perf_counter()
+elapsed = end_time - start_time
+elapsed_h = int(elapsed // 3600)
+elapsed_m = int(elapsed % 3600 // 60)
+elapsed_s = int(elapsed % 60)
+elapsed_ms = int(elapsed * 1000) % 1000
+print(" -> Elapsed time: "
+      + f"{elapsed_h:02d}:{elapsed_m:02d}:{elapsed_s:02d}.{elapsed_ms:03d}")
