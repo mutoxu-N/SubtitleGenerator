278f5832a964fe70cd919a7753fca7c62bcd27be
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-07-22 09:36:28 +0900

    SRTファイルの出力

--- main.py
+++ main.py
@@ -19,10 +19,30 @@ model = WhisperModel(MODEL.value, device="cuda", compute_type="float16")
 segments, _ = model.transcribe(INPUT_FILE, beam_size=5)
 
 # save to file
-with open("result.out", "w") as f:
-    text = ""
-    for segment in segments:
-        f.write("[%.2fs -> %.2fs] %s\n" %
-                (segment.start, segment.end, segment.text))
+text = ""
+with open("result.srt", "w") as f:
+    for i, segment in enumerate(segments):
+        start_h = int(segment.start) // 3600
+        start_m = int(segment.start) % 3600 // 60
+        start_s = int(segment.start) % 60
+        start_ms = int(segment.start * 1000) % 1000
+
+        end_h = int(segment.end) // 3600
+        end_m = int(segment.end) % 3600 // 60
+        end_s = int(segment.end) % 60
+        end_ms = int(segment.end * 1000) % 1000
+
+        f.write(f"{i}\n")
+        f.write("%2d:%02d:%02d,%03d --> %2d:%02d:%02d,%03d\n" %
+                (start_h, start_m, start_s, start_ms, end_h, end_m, end_s, end_ms))
+        f.write(f"{segment.text}\n")
+        f.write("\n")
         text += segment.text
-    print(text)
+
+with open("result.out", "w") as f:
+    f.write(text + "\n\n")
+
+    for segment in segments:
+        f.write(f"[{segment.start}s -> {segment.end}s] {segment.text}\n")
+print(text)
+print("Done!")
